3b587eebbcac4961717aa176d85e445ecbe9ea06
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2025-07-13 19:13:10 -0400

    Update Character.hx

--- source/Character.hx
+++ source/Character.hx
@@ -125,7 +125,7 @@ class Character extends FlxSprite {
 
 				flipX = true;
 			case 'glitch':
-                                frames = Paths.getSparrowAtlas('characters/Glitch', 'shared');
+                                frames = Paths.getSparrowAtlas('characters/Glitch_assets', 'shared');
 				animation.addByPrefix('idle', 'Idle', 24, false);
 				animation.addByIndices('singUP', 'Up', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23], "", 24, false);
 				animation.addByIndices('singDOWN', 'Down', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23], "", 24, false);
